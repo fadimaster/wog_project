0fe512732fb7d53a0ecf5a5427e57b04b96d461d
Fadi, Hinnawi <fadimaster88@gmail.com>
2024-03-26 03:13:31 +0200

    Jenkinsfile 2

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,25 +1,8 @@
-from selenium import web rowser
+from selenium import webdriver
 
-# URL to be opened in Chrome
-url_chrome = 'http://www.walla.co.il'
-
-# URL to be opened in Firefox
-url_firefox = 'http://www.ynet.co.il'
-
-# Path to the Chrome browser's executable file
-# Note: You need to replace this with the actual path of Chrome on your system
-chrome_path = '`C:\Program Files\Google\Chrome\Application\chrome.exe %s'
-
-# Path to the Firefox browser's executable file
-# Note: You need to replace this with the actual path of Firefox on your system
-firefox_path = 'C:\Program Files\Mozilla Firefox\firefox.exe %s'
-
-# Register the browsers
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
-webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
-
-# Open URL in Chrome
-webbrowser.get('chrome').open(url_chrome)
-
-# Open URL in Firefox
-webbrowser.get('firefox').open(url_firefox)
+driver = webdriver.Chrome(executable_path="C:\Program Files\Google\Chrome\Application\chrome.exe %s")
+driver.get("http://www.ynet.co.il")
+title = driver.title
+driver.refresh()
+if title==driver.title:
+    print("equal")
