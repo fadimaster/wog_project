80e4831efd88833ff541b7354f706ea5cf7d1ff6
Fadi, Hinnawi <fadimaster88@gmail.com>
2024-03-27 09:22:38 +0200

    Jenkinsfile 2

--- tests/e2e.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="C:\Program Files\Google\Chrome\Application\chrome.exe %s")
-driver.get("http://www.ynet.co.il")
-title = driver.title
-driver.refresh()
-if title==driver.title:
-    print("equal")
